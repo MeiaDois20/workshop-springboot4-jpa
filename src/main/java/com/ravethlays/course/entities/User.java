c97d9fe779724bd048bfb54504a71d513a05b754
MeiaDois20 <ravelthalys66@proton.me>
2026-03-15 17:43:05 -0300

    H2 database, test profile, Jpa

--- src/main/java/com/ravethlays/course/entities/User.java
+++ src/main/java/com/ravethlays/course/entities/User.java
@@ -1,14 +1,22 @@
 package com.ravethlays.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
+    @Column(unique = true)
     private String phone;
     private String password;
 
